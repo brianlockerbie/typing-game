b64969b1f07e24fd19528e9d713ccfaec7ca2756
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-09-30 17:26:26 -0400

    Added States/useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Words from "./components/Words";
 import "./App.css";
 
 const App = () => {
+  const [word, setWord] = useState(Words);
+  const [newWord,setnewWord] = useState(word[0]);
+  const [disabled, setDisabled] = useState(true);
+
   return (
     <div className="App">
-      <h1>hello</h1> 
+      <h1>Welcome</h1> 
     </div>
   );
-}
+};
 
 export default App;
