9bfc9e03c36fa0266d0c18de86829f7bc2839f12
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-09-30 17:49:59 -0400

    Added Typeracer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Words from "./components/Words";
+import Container from './components/Container';
 import "./App.css";
 
 const App = () => {
@@ -13,9 +14,13 @@ const App = () => {
   const [inputValue, setInputValue] = useState('');
   const [animation, setAnimation] = useState(null);
   
+  let randomWord = Math.floor(Math.random() * word.length);
+
   return (
     <div className="App">
-      <h1>Welcome</h1> 
+      <Container>
+        
+      </Container> 
     </div>
   );
 };
---
+++ src/components/Typeracer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button';
+
+const Typeracer = () => {
+
+    return(
+        <div className="typeRacer">
+          <div className="wordOutput">
+            <p>New Word</p>
+          </div>
+        </div>
+    );
+};
