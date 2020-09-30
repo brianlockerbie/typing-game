546a07b3cf0b6448d5d82ebe31aba0b9d4022e0b
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-09-30 17:32:00 -0400

    Added more states

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ const App = () => {
   const [word, setWord] = useState(Words);
   const [newWord,setnewWord] = useState(word[0]);
   const [disabled, setDisabled] = useState(true);
-
+  const [currentResults, setCurrentResults] = useState([]);
+  const [wrongResults, setWrongResults] = useState([]);
+  const [countCorrect, setCountCorrect] = useState(0);
+  const [time, setTime] = useState(30);
+  const [inputValue, setInputValue] = useState('');
+  const [animation, setAnimation] = useState(null);
+  
   return (
     <div className="App">
       <h1>Welcome</h1> 
